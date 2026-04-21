55efae946beecb807e2ffe1c283345dd8c803f67
BrStrategies-bg <178100295+BrStrategies-bg@users.noreply.github.com>
2026-04-21 01:33:09 +0000

    fix: handle missing .env files without throwing error

--- packages/excalidraw/env.cjs
+++ packages/excalidraw/env.cjs
@@ -1,18 +1,17 @@
 const dotenv = require("dotenv");
-const { readFileSync } = require("fs");
+const { readFileSync, existsSync } = require("fs");
 const pkg = require("./package.json");
+
 const parseEnvVariables = (filepath) => {
   let envVars = {};
-  try {
+  if (existsSync(filepath)) {
     envVars = Object.entries(dotenv.parse(readFileSync(filepath))).reduce(
-    (env, [key, value]) => {
-      env[key] = value;
-      return env;
-    },
-    {},
-  );
-  } catch (e) {
-    // If file doesn't exist, just use empty object
+      (env, [key, value]) => {
+        env[key] = value;
+        return env;
+      },
+      {},
+    );
   }
 
   envVars.PKG_NAME = pkg.name;
